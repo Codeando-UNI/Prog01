c2b0fad892e6301fb1ec310d8efd5697085e681c
AndresFernando01 <andres.castillo.1643@gmail.com>
2017-06-19 18:21:25 -0500

    andres

---
+++ relax/menu.php
@@ -0,0 +1,29 @@
+		<div id="page-wrapper">
+			<!-- Header -->
+				<header id="header">
+					<h1 id="logo"><a href="index.html">SmarthLight</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Inicio</a></li>
+							<li>
+								<a href="#">Nosotros</a>
+								<ul>
+									<li><a href="left-sidebar.html">¿Quiénes somos?</a></li>
+									<li><a href="right-sidebar.html">Galería</a></li>
+									<li>
+										<a href="#">Otros Proyectos</a>
+										<ul>
+											<li><a href="#">Option 1</a></li>
+											<li><a href="#">Option 2</a></li>
+											<li><a href="#">Option 3</a></li>
+											<li><a href="#">Option 4</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							<li><a href="#">Registro</a></li>
+							<li><a href="#" class="button special">Iniciar sesión</a></li>
+						</ul>
+					</nav>
+				</header>
+		</div>
